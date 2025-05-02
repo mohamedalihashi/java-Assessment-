d9b4184cddd2754b9c222a44e67c40d6c1dbb1da
mohamedalihashi <maxamedcalixashi02@gmail.com>
2025-05-02 12:23:41 -0700

    Add files via upload

---
+++ C1220740/src/main/java/com/C1220740/C1220740/C1220740Application.java
@@ -0,0 +1,20 @@
+package com.C1220740.C1220740;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+public class C1220740Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1220740Application.class, args);
+	}
+
+
+}
---
+++ C1220740/src/test/java/com/C1220740/C1220740/C1220740ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C1220740.C1220740;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1220740ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
